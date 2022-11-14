7e71b646fe608ac7c6d729b8d9cdf55718578719
Thamyris <thamyriscabral@hotmail.com>
2022-11-14 16:19:34 -0300

    projeto feito usando JS

--- media.js
+++ media.js
@@ -1,4 +1,31 @@
+(function(){
+  const alunos = [
+    {nome: 'Carlos', notas: [10, 9.5, 10, 9.5]},
+    {nome: 'Thamyris', notas: [10, 10, 10, 10]},
+    {nome: 'Magnum', notas: [9.5, 9.5, 9.5, 9.5]},
+    {nome: 'Camila', notas: [10, 9.5, 10, 9.5]},
+    {nome: 'Adilma', notas: [8.0, 9.5, 9.0, 9.5]},
+    {nome: 'Eduarda', notas: [7.0, 8.0, 7.0, 8.5]},
+    {nome: 'Joao', notas: [6.0, 7.0, 8.0, 7.0]},
+  ];
 
+    alunos.forEach(aluno => {
+      aluno.media = divisao(aluno.notas[0], aluno.notas[1], aluno.notas[2], aluno.notas[3],)
+    })
+
+      const html = alunos.map(aluno => 
+        `<tr>
+      <td>${aluno.nome}</td>
+      <td>${aluno.notas[0]}</td> 
+      <td>${aluno.notas[1]}</td> 
+      <td>${aluno.notas[2]}</td> 
+      <td>${aluno.notas[3]}</td>
+      <td>${aluno.media}</td> 
+    </tr>
+      `).join('')
+
+      document.querySelector('tbody').innerHTML = html
+    })()
 
 function soma() {
   let numeros = [...arguments]
@@ -9,4 +36,4 @@ function soma() {
   function divisao(){
     return soma(...arguments) / arguments.length
 }
-let resultado = divisao(10, 9.5, 8, 10)
+let resultado = divisao()
